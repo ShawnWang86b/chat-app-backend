d64bcc40cb9e68c64dbf9a33e6fb1bd7809634e1
shawn.wang <shawn.wang86b@gmail.com>
2023-01-13 00:46:41 +1100

    feat: add jest and ts

--- config/allowedOrigins.js
+++
@@ -1,3 +0,0 @@
-const allowedOrigins = ["https://shawns-chat-app-frontend.onrender.com"];
-
-module.exports = allowedOrigins;
--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,6 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const generateToken = require("../config/generateToken");
-const User = require("../models/userModel");
+const User = require("../src/models/userModel");
 
 //user register
 const registerUser = asyncHandler(async (req, res) => {
